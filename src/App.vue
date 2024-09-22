67c084942c4ecde5a1280eecb1915f067a27b528
Mr1Ceng <1025993890@qq.com>
2024-09-22 21:25:55 +0800

    feat: clear default

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+
+const msg = 'Mr1Ceng';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="@/assets/avatar.png" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+      <h1 class="green">{{ msg }}</h1>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
